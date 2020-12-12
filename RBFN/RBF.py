c6d71f69233d8bffe0ddcb3a11b923d40dfdb030
Hayeong Choi <21800758@handong.edu>
2020-12-12 22:22:49 +0900

    Add main

--- RBFN/RBF.py
+++ RBFN/RBF.py
@@ -96,3 +96,65 @@ def mean_squared_error(predict, label):
     #     return error  
     return ((label-predict)**2).mean(axis=None)
         
+
+    
+if __name__ == '__main__':
+    train_x1, train_y1 = load_data("cis_train1.txt")
+    train_x2, train_y2 = load_data("cis_train2.txt")
+    test_x, test_y = load_data("cis_test.txt")
+
+    train_x1_1 = train_x1[np.where(train_y1==1)]
+    train_x1_0 = train_x1[np.where(train_y1==0)]
+
+    plt.scatter(train_x1_0[:,0], train_x1_0[:,1], c = "lightskyblue", label="outside")
+    plt.scatter(train_x1_1[:,0], train_x1_1[:,1], c = "lightcoral", label="inside")
+    plt.legend()
+    
+    plt.title('cis_train1 scatter')
+    plt.savefig('HW9/cis_train1 scatter.png', bbox_inches='tight')
+    plt.show()
+
+    test_x_1 = test_x[np.where(test_y==1)]
+    test_x_0 = test_x[np.where(test_y==0)]
+
+    plt.scatter(test_x_0[:,0], test_x_0[:,1], c = "lightskyblue", label="outside")
+    plt.scatter(test_x_1[:,0], test_x_1[:,1], c = "lightcoral", label="inside")
+
+    plt.legend()
+    plt.title('cis_test scatter')
+    plt.savefig('HW9/cis_test scatter.png', bbox_inches='tight')
+    plt.show()
+
+    knum = [2, 5, 10, 15, 20, 30, 40, 50, 55]
+
+    for i, k in enumerate(knum):
+        model = RBFN(hidden_layer=k, variance=2.)
+        model.fit(train_x1, train_y1)
+        predictions, centers = model.predict(test_x)
+
+        predict=[]
+        for i in predictions:
+            if(i > 0.5):
+                predict.append(1)
+            else:
+                predict.append(0)
+
+        predict = np.array(predict)
+        correct = 0
+
+        for i in range(test_y.size):
+            if(test_y[i] == predict[i]):
+                correct = correct + 1
+        accuracy = correct/test_y.size
+
+        predict_1 = test_x[np.where(predict==1)]
+        predict_0 = test_x[np.where(predict==0)]
+
+        plt.scatter(predict_0[:,0], predict_0[:,1], c = "lightskyblue", label="outside")
+        plt.scatter(predict_1[:,0], predict_1[:,1], c = "lightcoral", label="inside")
+        plt.legend()
+
+        plt.title('Basis function =%d prediction scatter' %(k))
+        plt.savefig('HW9/k={}_prediction scatter.png'.format(k), bbox_inches='tight')
+        plt.show()
+        print("Basis function =", k, ", accuracy: ", accuracy)   
